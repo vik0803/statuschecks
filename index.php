311d57d2c05abd2ee17985b2a1eb3eab73fe6a7e
Benjamin Townsend <benjamin@bentownsend.com>
2015-07-10 11:31:23 -0400

    initial index.php with class autoloader

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Status Checks - PHP Server/Network Status Monitor and Notifications
+ * http://statuschecks.net
+ * Author: Ben Townsend
+ * Licensed Under: GNU GENERAL PUBLIC LICENSE V2
+ * Date: 7/10/2015
+ * Time: 9:49 AM
+ */
+
+/**
+ * Auto load magic function for all classes
+ * @param $class_name class to be loaded
+ */
+function __autoload($class_name) {
+    include 'class/class.' . $class_name . '.php';
+}
